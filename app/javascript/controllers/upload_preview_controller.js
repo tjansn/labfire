f54b1254715f065262e819dc5fd288a74b24e084
Tom <github@jansn.de>
2026-05-12 17:37:57 +0200

    Polish avatar upload controls

--- app/javascript/controllers/upload_preview_controller.js
+++ app/javascript/controllers/upload_preview_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "image", "input" ]
+  static targets = [ "image", "input", "placeholder" ]
 
   previewImage() {
     const file = this.inputTarget.files[0]
@@ -9,6 +9,8 @@ export default class extends Controller {
     if (file) {
       this.imageTarget.src = URL.createObjectURL(this.inputTarget.files[0]);
       this.imageTarget.onload = () => { URL.revokeObjectURL(this.imageTarget.src) }
+      if (this.hasPlaceholderTarget) this.placeholderTarget.hidden = true
+      this.element.classList.remove("gl-avatar-upload--empty", "gl-join-avatar-upload--empty")
     }
   }
 }
